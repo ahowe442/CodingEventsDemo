c95d16aa45119a13699d0342cfdeedadc9729cff
Abby Howe <howe.abigail@gmail.com>
2020-11-22 21:45:02 -0600

    Model binding, Created a data layer

--- CodingEvents/Controllers/EventsController.cs
+++ CodingEvents/Controllers/EventsController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodingEvents.Data;
+using CodingEvents.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,13 +13,15 @@ namespace CodingEvents.Controllers
     public class EventsController : Controller
     {
         //static private List<string> Events = new List<string>();
-        static private Dictionary<string, string> Events = new Dictionary<string, string>();
+        //static private Dictionary<string, string> Events = new Dictionary<string, string>();
+        static private List<Event> Events = new List<Event>();
+
         // GET: /<controller>/
         [HttpGet]
         public IActionResult Index()
         {
 
-            ViewBag.events = Events;
+            ViewBag.events = EventData.GetAll();
 
             return View();
         }
@@ -31,9 +35,27 @@ namespace CodingEvents.Controllers
 
         [HttpPost]
         [Route("/Events/Add")]
-        public IActionResult NewEvent(string name, string description)
+        public IActionResult NewEvent(Event newEvent)
         {
-            Events.Add(name, description);
+            EventData.Add(newEvent);
+
+            return Redirect("/Events");
+        }
+
+        public IActionResult Delete()
+        {
+            ViewBag.events = EventData.GetAll();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int[] eventIds)
+        {
+            foreach(int eventId in eventIds)
+            {
+                EventData.Remove(eventId);
+            }
+
             return Redirect("/Events");
         }
     }
---
+++ CodingEvents/Data/EventData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using CodingEvents.Models;
+
+namespace CodingEvents.Data
+{
+    public class EventData
+    {
+        //Store the events
+        static private Dictionary<int, Event> Events = new Dictionary<int, Event>();
+
+        //Add events
+        public static void Add(Event newEvent)
+        {
+            Events.Add(newEvent.Id, newEvent);
+        }
+
+        //GetAll - retrieve the events.
+        public static IEnumerable<Event> GetAll()
+        {
+            return Events.Values;
+        }
+
+        //GetById - retrieve by a single event by id. 
+        public static Event GetById(int id)
+        {
+            return Events[id];
+        }
+
+        //Remove
+        public static void Remove(int id)
+        {
+            Events.Remove(id);
+        }
+
+    }
+}
